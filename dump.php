48e2b825e12bf4cc3e553ed0352fd81470d406a0
Leon Blakey <lord.quackstar@gmail.com>
2012-10-22 09:10:37 -0400

    New output format with multiple tables

--- dump.php
+++ dump.php
@@ -13,21 +13,50 @@ if(isset($argv[1]) && strtolower($argv[1]) == "config") {
 	graph_category network
 	graph_info This graph shows the total traffic for $host
 	*/
-	$response = queryMiner("gpucount");
-	$numCards = $response["GPUS"][0]["Count"];
 	
-	$output = array();
-	for($i = 0; $i <= $numCards; $i++) {
-		$output[$i] = array(
-			"multigraph" => "miner_gpu" . $i,
-			"graph_title" => "GPU $i Mining Stats",
+	$output = array(
+		"hashes" => array(
+			"multigraph" => "miner_hashes",
+			"graph_title" => "GPU Hash Rate",
 			"graph_order" => "recv send",
 			//graph_args --base 1000
 			"graph_vlabel" => "MegaHashes per \${graph_period}",
 			"graph_category" => "sensors",
-			"graph_info" => "Statistics of GPU $i"
-		);
+			"graph_info" => "Statistics of GPU Hash Rate"
+		), "cooling" => array (
+			"multigraph" => "miner_cooling",
+			"graph_title" => "GPU Cooling stats",
+			"graph_order" => "recv send",
+			//graph_args --base 1000
+			"graph_vlabel" => "Temp(C) and Fan(%) per \${graph_period}",
+			"graph_category" => "sensors",
+			"graph_info" => "Statistics of GPU cooling"
+		), "int" => array (
+			"multigraph" => "miner_int",
+			"graph_title" => "GPU Intensity",
+			"graph_order" => "recv send",
+			//graph_args --base 1000
+			"graph_vlabel" => "Intensity at \${graph_period}",
+			"graph_category" => "sensors",
+			"graph_info" => "Statistics of GPU intensity"
+		)
+	);
+
+	//Create fields for each graph
+	$response = queryMiner("gpucount");
+	$numCards = $response["GPUS"][0]["Count"];
+	for($i = 0; $i < $numCards; $i++) {
+		$output["hashes"]["gpu{$i}_5s.label"] = "GPU $i 5s average MegaHashes/sec";
+		$output["hashes"]["gpu{$i}_avg.label"] = "GPU $i All time average MegaHashes/sec";
+		$output["cooling"]["gpu{$i}_temp.label"] = "GPU $i Temp (Celcius)";
+		$output["cooling"]["gpu{$i}_fan.label"] = "GPU $i Fan Percent";
+		$output["cooling"]["gpu{$i}_fan.min"] = "0";
+		$output["cooling"]["gpu{$i}_fan.max"] = "100";
+		$output["int"]["gpu{$i}_int.label"] = "GPU $i intensity";
+		$output["int"]["gpu{$i}_int.min"] = "0";
+		$output["int"]["gpu{$i}_int.max"] = "14";
 	}
+	
 	output($output);
 	exit(0);
 }
@@ -65,16 +94,15 @@ $response = queryMiner("devs");
 [Intensity] => 9
 */
 $output = array();
+$output["hashes"]["multigraph"] = "miner_hashes";
+$output["cooling"]["multigraph"] = "miner_cooling";
+$output["int"]["multigraph"] = "miner_int";
 foreach($response["DEVS"] as $value) {
 	$id = $value["ID"];
-	$output[$id] = array(
-		"multigraph" => "miner_gpu" . $i,
-		"hash5s.value" => $value["MHS 5s"],
-		"hashavg.value" => $value["MHS av"],
-		"intensity.value" => $value["Intensity"],
-		"fan.value" => $value["Fan Percent"],
-		"temp.value" => $value["Temperature"],
-		"utility" => $value["Utility"],
-	);
+	$output["hashes"]["gpu{$id}_5s.value"] = $value["MHS 5s"];
+	$output["hashes"]["gpu{$id}_avg.value"] = $value["MHS av"];
+	$output["cooling"]["gpu{$id}_temp.value"] = $value["Temperature"];
+	$output["cooling"]["gpu{$id}_fan.value"] = $value["Fan Percent"];
+	$output["int"]["gpu{$id}_int.value"] = $value["Intensity"];
 }
 output($output);
